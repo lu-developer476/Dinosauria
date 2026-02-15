1fe950b1f327f49786ab233460e2e2072908f5bf
Lu <117615336+lu-developer476@users.noreply.github.com>
2026-02-15 04:09:48 -0300

    types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,10 @@
-export interface DinoCard {
+export type Era = "Triásico" | "Jurásico" | "Cretácico";
+
+export type DinoCard = {
   id: string;
   name: string;
   era: Era;
   diet: "Herbívoro" | "Carnívoro" | "Omnívoro";
   size: "Pequeño" | "Mediano" | "Grande" | "Colosal";
   description: string;
-}
+};
