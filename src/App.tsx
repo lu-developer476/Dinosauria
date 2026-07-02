e4868266682a50e0cb9a90bb79357b3a1af97409
Lu <117615336+lu-developer476@users.noreply.github.com>
2026-07-02 02:44:47 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -662,7 +662,7 @@ const gallery = useMemo(
 
       <footer className="footer">
         <div className="container footer-inner">
-          <small>© {new Date().getFullYear()} {t.rights} • Built with HTML5, CSS3, JavaScript, TypeScript, CoffeeScript, React.js • UX/UI Interface • Deployed on Vercel ®</small>
+          <small>© {new Date().getFullYear()} {t.rights} • Built with HTML5, CSS3, JavaScript, TypeScript, CoffeeScript & React.js • UX/UI Interface • Deployed on Vercel ®</small>
         </div>
       </footer>
     </>
